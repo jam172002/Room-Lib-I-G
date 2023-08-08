0587d9db6174308d7ac270b4e7dd3d5cea8a98dc
jamshaid <jam172002@gmail.com>
2023-08-08 20:59:03 +0500

    The Dao class is created

---
+++ app/src/main/java/com/example/roomlib_i/DAO.java
@@ -0,0 +1,30 @@
+package com.example.roomlib_i;
+
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.RoomDatabase;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface DAO {
+
+    //we need the return type here because the data we get here will be saved in data type
+    @Query("select * from MY_Table")
+    List<Entity> getAll();
+
+    //this method need data to be inserted so we will pass the data to it
+    //and I am passing it in form of my entity data type
+    @Insert
+    void insertData(Entity entity);
+
+    @Update
+    void updateData(Entity entity);
+
+    @Delete
+    void deleteData(Entity entity);
+}
--- app/src/main/java/com/example/roomlib_i/Entity.java
+++ app/src/main/java/com/example/roomlib_i/Entity.java
@@ -2,6 +2,7 @@ package com.example.roomlib_i;
 
 
 import androidx.room.ColumnInfo;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @androidx.room.Entity(tableName = "MY_Table")
@@ -20,4 +21,55 @@ public class Entity {
     @ColumnInfo(name = "Pages")
     private String pNumber;
 
+    //now the main constructor
+
+
+    public Entity(int id, String name, String author, String pNumber) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.pNumber = pNumber;
+    }
+
+    //this is not main constructor therefore it is annotated so IDE could see which is default constructor
+    @Ignore
+    public Entity(String name, String author, String pNumber) {
+        this.name = name;
+        this.author = author;
+        this.pNumber = pNumber;
+    }
+
+    //the getter and setter work as same as constructor but still we need when one or few variables need to be get and set values
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getpNumber() {
+        return pNumber;
+    }
+
+    public void setpNumber(String pNumber) {
+        this.pNumber = pNumber;
+    }
 }
